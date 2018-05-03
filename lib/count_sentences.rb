5ecd9ce5eb9d1e3e6dd8d137c9ead3e74a9abf5d
Monica Marziano <mmarziano1@gmail.com>
2018-05-03 09:20:56 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,9 +16,9 @@ class String
 
   def count_sentences
     delimiters = self.strip.split(/\w+/)
-    sentences = []
-    delimiters.each {|x| sentences << x if x == "." || x == "?" || x == "!"}
-      sentences
+    #sentences = []
+    #delimiters.each {|x| sentences << x if x == "." || x == "?" || x == "!"}
+      #sentences
     end
    
   
